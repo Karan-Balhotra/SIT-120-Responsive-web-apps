71e1b4b9d418f699997f5619aaa668da4c71df5c
Karan-Balhotra <130816907+Karan-Balhotra@users.noreply.github.com>
2023-08-18 21:05:41 +1000

    first commit

---
+++ task 2.1c/test/script.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: {
+      input: '',
+      items: [
+          { name: 'Water' },
+          { name: 'Juice' },
+          { name: 'Cola' },
+          { name: 'Milk' }
+      ]
+  }
+});
